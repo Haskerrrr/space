ffa566a00d9302240051247c4687e242396d1fb0
Gusta <gustavorodrigues182@live.com>
2023-10-11 02:00:16 -0300

    Fix: settings

--- setup/settings.py
+++ setup/settings.py
@@ -127,11 +127,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#Upload de fotos:
+# Media
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
